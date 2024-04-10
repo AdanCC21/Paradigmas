24f80e9aace5c1435599dc8997f8a8e010e9a6d2
Andan <141715477+AdanCC21@users.noreply.github.com>
2024-04-10 10:50:42 -0700

    Datos personales y de contacto completos

--- CimaBank/main.py
+++ CimaBank/main.py
@@ -1,7 +1,7 @@
 import random
 import os
 
-class person:
+class Person:
     #datos personales
     _name=''
     _app=''
@@ -29,14 +29,16 @@ class person:
         self._pasword=pasword
         self._phone=phone
 
-class bank:
+class Bank:
     #datos bancarios
+    #titular Sera de la clase Person
     _noCuenta=''
     _saldo=0
     _clabe=''
 
-    def __bank_init__(self, num,_clabe):
+    def __init__(self,titular ,num,_clabe):
         #Numero de cuenta
+        self._titular=titular
         noCuenta4=random.randint(1000,9999)
         noCuenta8=random.randint(1000,9999)
         noCuenta12=random.randint(1000,9999)
@@ -55,40 +57,89 @@ class bank:
         self._clabe=str(clabe3)+str(clabe6)+str(clabe17)+str(clabe18)   
 
 
-def Menu():
-    print("Bienvenido al CimaBanck")
-    print("Porfavor seleccione una opcion")
-    print("1.-Iniciar sesion\t2.-Registrarse")
-
 def registrar():
     os.system('cls')
     print("Favor de ingresar los datos")
     
-    tname=input("Nombre")
-    tapp=input("Apellido paterno")
-    tapm=input("Apellido Materno")
+    tname=input("Nombre\n")
+    tapp=input("Apellido paterno\n")
+    tapm=input("Apellido Materno\n")
     os.system('cls')
 
     print('Fecha de nacimiento')
-    tday=input("Dia")
-    tmonth=input("Mes")#metodo de validacion para solo numeros
-    tyear=input("Year")
-    persona1=person(tname,tapp,tapm,tday,tmonth,tyear)
-    return persona1
+    tday=input("Dia\n")
+    tmonth=input("Mes\n")#metodo de validacion para solo numeros
+    tyear=input("Year\n")
 
+    #Pega los datos en una variable temporal
+    PerBase=Person(tname,tapp,tapm,tday,tmonth,tyear)
+    
+    #datos de contacto
+    tmail=input("Email\n")
+    pas=input("Contrasena\n")
+    phone=input("Phone number\n")
+    perBase.__count_init__(tmail,pas,phone)
+
+    #Banco
+    
+    
+    print("Datos copiados\n")
+    os.system("pause")
+
+    return PerBase
+
+def MenuLogin():
+    print("\tBienvenido al CimaBanck")
+    print(f'{'Porfavor seleccione una opcion':>35}\n')
+    print("1.-Iniciar sesion\t\t2.-Registrarse")
 
 #--------------------------MAIN---------------------------
 
+#-----Inicio de sesio-----
 op=1 #Variable para el ciclo
-persona1=person("","","","","","")
-persona1=registrar()
-print(persona1._name)
+perBase=Person("","","","","","") #persona base
 
+while op!=0: #ciclo de inicio de sesion
+    os.system("cls")
+    MenuLogin()
+    op=int(input())
 
-# while op!=0: #ciclo del usuario
-#     Menu()
-#     op=int(input())
+    if op ==1:
+        print("\tDatos personales")
+        print("Nombre :",PerBase._name, end=("\t"))
+        print("Apellido paterno :",PerBase._app, end=("\t"))
+        print("Apellido Materno :",PerBase._apm)
+        
+        print("")
+        print("Fecha de nacimiento")
+        print("Dia",PerBase._day,"Mes",PerBase._month,"Año",PerBase._year)
 
+        print("")
+        print("Correo",perBase._mail,"Contra",perBase._pasword,"Phone",perBase._phone)
+        
+        
+        os.system("pause")
+        op=0#una vez termine la operacion, inicamos la aplicacion
+        #Iniciar sesion
+    
+    if op == 2:
+        PerBase=registrar()
+        #Una vez termine de registrarse, debera de inciar sesion
+
+def MenuMain():
+    print("Bienvenido",perBase._name)
+    print("Que desea realizar hoy?")
+    print("1.-Ver saldo actual\t2.-Transaccion\t3.-Depositar\t4.-Retirar")
+    op=int(input())
+    return op
+
+def saldo():
+    print("saldo actual")
+
+op=1
+while(op!=0):
+    os.system("cls")
+    op=MenuMain()
     
 
 
