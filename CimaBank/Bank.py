9d2af9c1ff8d135e3da9e121f4d61e0ac3c578ea
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-04-10 19:41:38 -0700

    clases acomodadas

---
+++ CimaBank/Bank.py
@@ -0,0 +1,30 @@
+import Person
+import random
+
+class Bank:
+    #datos bancarios
+    #titular Sera de la clase Person
+    _noCuenta=''
+    _saldo=0
+    _clabe=''
+
+    def __init__(self,titular):
+        #Numero de cuenta
+        self._titular=titular
+        
+        noCuenta4=random.randint(1000,9999)
+        noCuenta8=random.randint(1000,9999)
+        noCuenta12=random.randint(1000,9999)
+        noCuenta16=random.randint(1000,9999)
+        self._noCuenta=str(noCuenta4)+str(noCuenta8)+str(noCuenta12)+str(noCuenta16)
+        
+        #saldo
+        self._saldo=0
+
+        #Generar clabe por partes
+        clabe3=random.randint(100,999)
+        clabe6=random.randint(100,999)
+        clabe17=random.randint(10000000000,99999999999)
+        clabe18=random.randint(1,9)
+
+        self._clabe=str(clabe3)+str(clabe6)+str(clabe17)+str(clabe18)
--- CimaBank/main.py
+++ CimaBank/main.py
@@ -1,109 +1,39 @@
 import random
 import os
 
-class Person:
-    #datos personales
-    _name=''
-    _app=''
-    _apm=''
-    _day=0
-    _month=0
-    _year=0
-    
-    def __init__(self, nombre, app, apm,day,month,year):
-        self._name=nombre
-        self._app=app
-        self._apm=apm
-        
-        self._day=day
-        self._month=month
-        self._year=year
-
-    #datos de cuenta
-    _mail=''
-    _pasword=''
-    _phone=''
-
-    def __count_init__(self, mail, pasword, phone):
-        self._mail=mail
-        self._pasword=pasword
-        self._phone=phone
-
-class Bank:
-    #datos bancarios
-    #titular Sera de la clase Person
-    _noCuenta=''
-    _saldo=0
-    _clabe=''
-
-    def __init__(self,titular):
-        #Numero de cuenta
-        self._titular=titular
-        
-        noCuenta4=random.randint(1000,9999)
-        noCuenta8=random.randint(1000,9999)
-        noCuenta12=random.randint(1000,9999)
-        noCuenta16=random.randint(1000,9999)
-        self._noCuenta=str(noCuenta4)+str(noCuenta8)+str(noCuenta12)+str(noCuenta16)
-        
-        #saldo
-        self._saldo=0
-
-        #Generar clabe por partes
-        clabe3=random.randint(100,999)
-        clabe6=random.randint(100,999)
-        clabe17=random.randint(10000000000,99999999999)
-        clabe18=random.randint(1,9)
-
-        self._clabe=str(clabe3)+str(clabe6)+str(clabe17)+str(clabe18)
-           
+#Clases
+import Person
+import Bank
 
 def registrar():
     os.system('cls')
-    print("Favor de ingresar los datos")
-    
-    tname=input("Nombre\n")
-    tapp=input("Apellido paterno\n")
-    tapm=input("Apellido Materno\n")
-    os.system('cls')
+    op=1
+    while op!= 0:
+        print("Favor de ingresar los datos")
+        
+        tname=input("Nombre\n")
+        tapp=input("Apellido paterno\n")
+        tapm=input("Apellido Materno\n")
+        os.system('cls')
 
-    print('Fecha de nacimiento')
-    tday=input("Dia\n")
-    tmonth=input("Mes\n")#metodo de validacion para solo numeros
-    tyear=input("Year\n")
+        print('Fecha de nacimiento')
+        tday=input("Dia\n")
+        tmonth=input("Mes\n")#metodo de validacion para solo numeros
+        tyear=input("Year\n")
 
-    #Pega los datos en una variable temporal
-    PerBase=Person(tname,tapp,tapm,tday,tmonth,tyear)
-    
-    #datos de contacto
-    tmail=input("Email\n")
-    pas=input("Contrasena\n")
-    phone=input("Phone number\n")
-    perBase.__count_init__(tmail,pas,phone)
-    
-    print("Datos copiados\n")
-    os.system("pause")
+        
+        #datos de contacto
+        os.system('cls')
+        tmail=input("Email\n")
+        pas=input("Contrasena\n")
+        phone=input("Phone number\n")
 
-    return PerBase
+        #Pega los datos en una variable temporal
+        PerBase=Person.Person(tname,tapp,tapm,tday,tmonth,tyear)
+        perBase.__count_init__(tmail,pas,phone)
 
-def MenuLogin():
-    print("\tBienvenido al CimaBanck")
-    print(f'{'Porfavor seleccione una opcion':>35}\n')
-    print("1.-Iniciar sesion\t\t2.-Registrarse")
-
-#--------------------------MAIN---------------------------
-
-#-----Inicio de sesio-----
-op=1 #Variable para el ciclo
-perBase=Person("","","","","","") #persona base
-
-while op!=0: #ciclo de inicio de sesion
-    os.system("cls")
-    MenuLogin()
-    op=int(input())
-
-    if op ==1:
-        print("\tDatos personales")
+        os.system("cls")
+        print("Estos son sus datos")
         print("Nombre :",PerBase._name, end=("\t"))
         print("Apellido paterno :",PerBase._app, end=("\t"))
         print("Apellido Materno :",PerBase._apm)
@@ -113,18 +43,56 @@ while op!=0: #ciclo de inicio de sesion
         print("Dia",PerBase._day,"Mes",PerBase._month,"Año",PerBase._year)
 
         print("")
-        print("Correo",perBase._mail,"Contra",perBase._pasword,"Phone",perBase._phone)
+        print("Correo",perBase._mail,"\tPasword",perBase._pasword,"\tPhone Number",perBase._phone)
+        
+        print("")
+        print("Desea continuar?")
+        print("1.-Continuar\t2.-Cancelar")
+        op=int(input())
+        op=op-1
+    
+    
+    
+    print("Datos copiados\n")
+    return PerBase
+
+def MenuLogin():
+    print("\tBienvenido al CimaBanck")
+    print(f'{'Porfavor seleccione una opcion':>35}\n')
+    print("1.-Iniciar sesion\t\t2.-Registrarse")
+
+#--------------------------INICIO DE SESION---------------------------
+
+op=1 #Variable para el ciclo
+perBase=Person.Person("","","","","","") #persona base
+
+while op!=0: #ciclo de inicio de sesion
+    os.system("cls")
+    MenuLogin()
+    op=int(input())
+
+    if op ==1:
+        #Iniciar sesion
+        print("Ingrese su No.Cuenta")
         
         
         os.system("pause")
         op=0#una vez termine la operacion, inicamos la aplicacion
-        #Iniciar sesion
     
     if op == 2:
+        #Registrarse
         PerBase=registrar()
+        BankBase=Bank.Bank(perBase)
+        
+        print("")
+        print("Estos son sus datos bancarios")
+        print("clabe",BankBase._clabe,"\tcuenta",BankBase._noCuenta, "\tsaldo",BankBase._saldo)
+
+        os.system("pause")
         #Una vez termine de registrarse, debera de inciar sesion
 
 #-------------------------------------------------------------------------
+
 def MenuMain():
     print("Bienvenido",perBase._name)
     print("Que desea realizar hoy?")
@@ -132,13 +100,10 @@ def MenuMain():
     op=int(input())
     return op
 
-def saldo():
+def saldo(banco):
     print("saldo actual")
-#---------------------------------------------------------------------------
 
-BankBase=Bank(perBase)
-print("clabe",BankBase._clabe,"cuenta",BankBase._noCuenta, "saldo",BankBase._saldo)
-os.system("pause")
+#--------------------------INICIO---------------------------
 
 # op=1
 # while(op!=0):
